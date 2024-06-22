8586304c930ed95d2a151d2c698ee94b3419aa47
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-06-22 16:54:34 +0900

    fix to return grids

--- intuitive_understanding_of_linear_algebra/src/utils.py
+++ intuitive_understanding_of_linear_algebra/src/utils.py
@@ -2,7 +2,10 @@ import numpy as np
 from matplotlib.axes import Axes
 
 
-def plot_full_custom_2d_grid(ax: Axes, basis: np.array, xlim: tuple, ylim: tuple, grid_step: float=1.0):
+def plot_full_custom_2d_grid(
+    ax: Axes, basis: np.array, xlim: tuple, ylim: tuple, grid_step: float=1.0, linewidth: float=0.5, color: str = "gray"
+):
+    grid_lines = []
     
     x = np.arange(xlim[0], xlim[1] + grid_step, grid_step)
     y = np.arange(ylim[0], ylim[1] + grid_step, grid_step)
@@ -10,10 +13,13 @@ def plot_full_custom_2d_grid(ax: Axes, basis: np.array, xlim: tuple, ylim: tuple
     for xi in x:
         points = np.array([[xi, ylim[0]], [xi, ylim[1]]])
         transformed_points = points @ basis
-        ax.plot(transformed_points[:, 0], transformed_points[:, 1], color='gray', linestyle='--', linewidth=0.5)
+        line, = ax.plot(transformed_points[:, 0], transformed_points[:, 1], color=color, linestyle='--', linewidth=linewidth)
+        grid_lines.append(line)
     
     for yi in y:
         points = np.array([[xlim[0], yi], [xlim[1], yi]])
         transformed_points = points @ basis
-        ax.plot(transformed_points[:, 0], transformed_points[:, 1], color='gray', linestyle='--', linewidth=0.5)
-        
+        line, = ax.plot(transformed_points[:, 0], transformed_points[:, 1], color=color, linestyle='--', linewidth=linewidth)
+        grid_lines.append(line)
+    
+    return grid_lines
